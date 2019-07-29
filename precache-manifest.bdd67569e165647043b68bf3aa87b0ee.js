b6598b18284a34550c07f6a433893236b0be234f
unknown <NickDiz>
2019-07-28 22:47:25 -0400

    Updates

---
+++ precache-manifest.bdd67569e165647043b68bf3aa87b0ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08478a88fab8f404c3d0d6eec86f9de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caadfd986cfd7f1a5354",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "caadfd986cfd7f1a5354",
+    "url": "/static/js/main.2554797e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
